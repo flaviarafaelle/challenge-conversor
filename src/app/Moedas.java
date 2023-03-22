8a89bccccee1cad2b5d3588d77748e55279b71c7
Flavia <flaviarlimasantos@gmail.com>
2023-03-22 20:04:17 -0300

    feat: moedas

---
+++ src/app/Moedas.java
@@ -0,0 +1,18 @@
+package app;
+
+import javax.swing.*;
+
+public class Moedas {
+    
+    public String converter(String pais, double valor) {
+        
+        double valorConvertido = 0;
+        String text = null;
+
+        try {
+            switch(pais) {
+            case
+            }
+        }
+    }
+}
